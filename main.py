0616dfaa5c67c4316a1741daf5ec9e9632ba179b
LeonidK23 <kaida.leonid@gmail.com>
2019-07-18 16:48:37 +0200

    First version of UI kNN ready needs test

--- main.py
+++ main.py
@@ -4,37 +4,47 @@ import numpy as np
 from recommenders.mean_recommender import MeanRecommender
 from recommenders.random_recommender import RandomRecommender
 from recommenders.knn_user_user_recommender import kNNRecommenderUU
-# from recommenders.knn_user_item_recommender import kNNRecommenderUI
+from recommenders.knn_user_item_recommender import kNNRecommenderUI
 from recommenders.knn_item_item_recommender import kNNRecommenderII
 
 X_test = np.genfromtxt(os.path.join("data", "qualifying_blanc.csv"), delimiter=",", dtype=np.int)
 X_train = np.genfromtxt(os.path.join("data", "train.csv"), delimiter=",", dtype=np.int)
 
-clf_mean = MeanRecommender()
-clf_mean.fit(X_train)
-predictions_mean = clf_mean.predict(X_test)
+# clf_mean = MeanRecommender()
+# clf_mean.fit(X_train)
+# predictions_mean = clf_mean.predict(X_test)
+#
+# clf_rand = RandomRecommender()
+# clf_rand.fit(X_train)
+# predictions_rand = clf_rand.predict(X_test)
+#
+# clf_knn_UU = kNNRecommenderUU(3)
+# clf_knn_UU.fit(X_train)
+# predictions_kNN_UU = clf_knn_UU.predict(X_test)
+#
+# clf_knn_II = kNNRecommenderII(3)
+# clf_knn_II.fit(X_train)
+# predictions_kNN_II = clf_knn_II.predict(X_test)
 
-clf_rand = RandomRecommender()
-clf_rand.fit(X_train)
-predictions_rand = clf_rand.predict(X_test)
+clf_knn_UI = kNNRecommenderUI(3)
+clf_knn_UI.fit(X_train)
+predictions_kNN_UI = clf_knn_UI.predict(X_test)
 
-clf_knn = kNNRecommenderUU(3)
-clf_knn.fit(X_train)
-predictions_kNN_UU = clf_knn.predict(X_test)
-
-clf_knn = kNNRecommenderII(3)
-clf_knn.fit(X_train)
-predictions_kNN_II = clf_knn.predict(X_test)
-
-np.savetxt("data/qualifying_mean.csv", predictions_mean,
-           delimiter=",", newline="\n", encoding="utf-8")
-np.savetxt("data/qualifying_random.csv", predictions_rand,
-           delimiter=",", newline="\n", encoding="utf-8")
-np.savetxt("data/qualifying_knn_UU.csv", predictions_kNN_UU,
-          delimiter=",", newline="\n", encoding="utf-8")
-np.savetxt("data/qualifying_knn_II.csv", predictions_kNN_II,
-          delimiter=",", newline="\n", encoding="utf-8")
+# np.savetxt("data/qualifying_mean.csv", predictions_mean,
+#            delimiter=",", newline="\n", encoding="utf-8")
+# np.savetxt("data/qualifying_random.csv", predictions_rand,
+#            delimiter=",", newline="\n", encoding="utf-8")
+# np.savetxt("data/qualifying_knn_UU.csv", predictions_kNN_UU,
+#           delimiter=",", newline="\n", encoding="utf-8")
+# np.savetxt("data/qualifying_knn_II.csv", predictions_kNN_II,
+#           delimiter=",", newline="\n", encoding="utf-8")
+# np.savetxt("data/qualifying_knn_UI.csv", predictions_kNN_UI,
+#           delimiter=",", newline="\n", encoding="utf-8")
 
 # TODO: add train_test_split
 # TODO: cross_validation,
 # TODO: correct packaging(add __init__ file for the project)
+
+# TODO: try floor rounding
+# TODO: add weighting of users and items
+# TODO: combine user-user and item-item collaboration
--- recommenders/knn_user_item_recommender.py
+++ recommenders/knn_user_item_recommender.py
@@ -13,17 +13,17 @@ class kNNRecommenderUI:
     def fit(self, train_data):
         self.global_mean = np.mean(train_data[:, 2])
         sparse_mat = csr_matrix((train_data[:, 2], (train_data[:, 0], train_data[:, 1])))
-        self.train_data = sparse_mat.todense()
+        self.train_data = sparse_mat
         simil_matrix_users = cosine_similarity(sparse_mat)
         np.fill_diagonal(simil_matrix_users, -1)
         k_nearest_users_dists = np.sort(simil_matrix_users, axis=1)[:, -self.n_neighbours:]
-        self.k_nearest_users = np.array([self.get_neighbour_indices(simil_matrix_users[i, :], k_nearest_users_dists[i, :])
+        self.k_nearest_users = np.array([self.get_neighbour_indices(simil_matrix_users[i, :], k_nearest_users_dists[i, :]) + [i]
                       for i in range(k_nearest_users_dists.shape[0])])
         # Find nearest neighbours for each item
         simil_matrix_items = cosine_similarity(sparse_mat.transpose())
         np.fill_diagonal(simil_matrix_items, -1)
         k_nearest_items_dists = np.sort(simil_matrix_items, axis=1)[:, -self.n_neighbours:]
-        self.k_nearest_items = np.array([self.get_neighbour_indices(simil_matrix_items[i, :], k_nearest_items_dists[i, :])
+        self.k_nearest_items = np.array([self.get_neighbour_indices(simil_matrix_items[i, :], k_nearest_items_dists[i, :]) + [i]
                       for i in range(k_nearest_items_dists.shape[0])])
 
     def get_neighbour_indices(self, user, distances):
@@ -43,10 +43,15 @@ class kNNRecommenderUI:
         users_items_to_fill = self.collect_user_items_dict(test_data)
         predictions = []
         for user in users_items_to_fill:
-            neighbours = np.take(self.train_data, self.k_neighbours[user], axis=0)
             for item in users_items_to_fill[user]:
-                item_prediction = int(np.ceil(np.mean(neighbours[:, item])))
-                predictions.append([user, item, item_prediction if item_prediction!=0 else
-                                    self.global_mean])
+                # neighbours_users = np.take(self.train_data, self.k_nearest_users[user], axis=0) # old version
+                # neighbours_users = self.train_data[np.ix_(self.k_nearest_users[user], self.k_nearest_items[item])] # with dense matrix
+                neighbours_users = self.train_data[np.ix_(self.k_nearest_users[user], self.k_nearest_items[item])].data
+                # select from training data nearest users and nearest items to the unknow item/user
+                # except the unknown value of item to fill
+                # item_prediction = np.mean(np.asarray(neighbours_users).flatten()[:-1]) # try this with dense matrix
+                item_prediction = self.global_mean if neighbours_users.shape[0] == 0 else np.mean(neighbours_users)
+                # item_prediction = int(np.ceil(nearest_users_items.mean())) # Try: not round or use floor
+                predictions.append([user, item, item_prediction])
 
         return predictions
