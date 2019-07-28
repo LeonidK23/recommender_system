dd635ee411a7cc946a581771dc7bcd3ecaf5d551
Leonid <kli.voyage@gmail.com>
2019-07-28 23:35:06 +0200

    Collect similar ratings for each pair of items

--- recommenders/knn_user_item_recommender.py
+++ recommenders/knn_user_item_recommender.py
@@ -69,77 +69,92 @@ class kNNRecommenderUI:
     #     return np.array(predictions)
 
     def pearson_corr(self, mat):
-        mu = np.zeros((mat.shape[0], 1))
+        mat_ok = csr_matrix((mat[:, 2], (mat[:, 0], mat[:, 1]))).T
+        mat_1 = csr_matrix((mat[:, 2]+1, (mat[:, 0], mat[:, 1]))).T
+        mu = np.zeros((mat_ok.shape[0], 1))
         non_zeros_items_indices = []
-        corr_mat = np.zeros((mat.shape[0], mat.shape[0]))
+        corr_mat = np.zeros((mat_ok.shape[0], mat_ok.shape[0]))
         corr_mat[:] = np.nan
-        non_zeros_items_indices = np.array(list(set(mat.nonzero()[0])))
-        non_zeros_items = mat[non_zeros_items_indices, :]
+        non_zeros_items_indices = np.array(list(set(mat_ok.nonzero()[0])))
+        non_zeros_items = mat_ok[non_zeros_items_indices, :]
         non_zeros_mu = mu[non_zeros_items_indices]
         centered_items = non_zeros_items - non_zeros_mu
-        for i in range(mat.shape[0]):
-            mu[i] = np.nan if mat[i, :].data.shape[0]==0 else np.mean(mat[i, :].data)
-        for i in range(mat.shape[0]):
-            I_u = mat[i, :].nonzero()[1]
-            if I_u.shape[0] > 0:
-                # center_i = mat[i, :] - mu[i]
-                center_i = mat[i, :]
-                numerator = np.sum(np.multiply(center_i, centered_items), axis=1)
-                denumerator = np.sqrt(np.sum(np.square(center_i)))*np.sqrt(np.sum(np.square(centered_items), axis=1))
-                corr_mat[i, non_zeros_items_indices] = np.divide(numerator, denumerator).reshape(-1)
-            else:
-                corr_mat[i, :] = np.nan
+        I_v = []
+        for i in range(mat_ok.shape[0]):
+            mu[i] = np.nan if mat_ok[i, :].data.shape[0]==0 else np.mean(mat_ok[i, :].data)
+            I_v.append(mat_1[i, :].nonzero()[1])
 
-        return corr_mat
+        for i in range(mat_ok.shape[0]):
+            I_u = mat_1[i+1200, :].nonzero()[1]
+            for j in range(len(I_v)):
+                print(I_u.shape)
+                print(I_v[j].shape)
+                I_u_I_v = np.intersect1d(I_u, I_v[j])
+                print(I_u_I_v.shape)
+                ratings_u = mat_1[i+1200, I_u_I_v].data - 1
+                ratings_v = mat_1[j, I_u_I_v].data - 1
+                print(ratings_u)
+                print(ratings_v)
+                print('-----------------')
+            # if I_u.shape[0] > 0:
+            #     # center_i = mat[i, :] - mu[i]
+            #     center_i = mat[i, :]
+            #     numerator = np.sum(np.multiply(center_i, centered_items), axis=1)
+            #     denumerator = np.sqrt(np.sum(np.square(center_i)))*np.sqrt(np.sum(np.square(centered_items), axis=1))
+            #     corr_mat[i, non_zeros_items_indices] = np.divide(numerator, denumerator).reshape(-1)
+            # else:
+            #     corr_mat[i, :] = np.nan
+        #
+        # return corr_mat
 
     def fit(self, train_data):
         # self.train_data = csr_matrix((train_data[:, 2], (train_data[:, 0], train_data[:, 1]))).todense().T
         # self.sim_mat_items = cosine_similarity(self.train_data)
         # np.fill_diagonal(self.sim_mat_items, -1)
-        self.train_data = csr_matrix((train_data[:, 2], (train_data[:, 0], train_data[:, 1]))).T
-        self.sim_mat_items = self.pearson_corr(self.train_data)
+        self.train_data = csr_matrix((train_data[:, 2]+1, (train_data[:, 0], train_data[:, 1]))).T
+        self.sim_mat_items = self.pearson_corr(train_data)
 
     def predict(self, test_data):
         # The best config
-        # items_user = self.collect_items_user_dict(test_data)
-        # predictions = []
-        # for item in items_user:
-        #     closest_dists = np.sort(self.sim_mat_items[item, :])[-self.n_neighbours:]
-        #     nearest_items = []
-        #     for dist in closest_dists:
-        #         nearest_items.append(np.where(self.sim_mat_items[item, :] == dist)[0][0])
-        #     nearest_feedbacks = self.train_data[np.array(nearest_items), :]
-        #     for user in items_user[item]:
-        #         prediction = np.sqrt(np.sum(np.square(nearest_feedbacks[:, user])))/self.n_neighbours*2.5
-        #         # if nearest_feedbacks[:, user].data.shape[0] == 0:
-        #         #     prediction = 0
-        #         # else:
-        #         #     prediction = np.mean(nearest_feedbacks[:, user].data)
-        #         predictions.append([user, item, prediction])
-
-
         items_user = self.collect_items_user_dict(test_data)
         predictions = []
         for item in items_user:
-            dists = np.sort(self.sim_mat_items[item, :])
-            num_dists = dists[~np.isnan(dists)]
-            nearest_dists = num_dists[-self.n_neighbours:]
-            nearest_items_inds = []
-            for dist in nearest_dists:
-                nearest_items_inds.append(np.where(self.sim_mat_items[item, :] == dist)[0][0])
-            nearest_items = self.train_data[np.array(nearest_items_inds), :]
+            closest_dists = np.sort(self.sim_mat_items[item, :])[-self.n_neighbours:]
+            nearest_items = []
+            for dist in closest_dists:
+                nearest_items.append(np.where(self.sim_mat_items[item, :] == dist)[0][0])
+            nearest_feedbacks = self.train_data[np.array(nearest_items), :]
             for user in items_user[item]:
-                nearest_ratings = nearest_items[:, user].data
-                ratings_inds = nearest_items[:, user].nonzero()[0]
-                # print(nearest_dists)
-                # print(nearest_ratings - np.mean(nearest_ratings))
-                # print(nearest_ratings)
-                # print(ratings_inds)
-                # print(nearest_dists)
-                # print(nearest_dists[ratings_inds])
-                # print(np.sum(nearest_dists[ratings_inds]))
-                # print('----------------------------------------')
-                prediction = np.sum(np.multiply(nearest_dists[ratings_inds], nearest_ratings))/np.sum(nearest_dists[ratings_inds])
-                predictions.append([user, item, np.mean(self.train_data[item, :]) if np.isnan(prediction) else prediction])
+                prediction = np.sqrt(np.sum(np.square(nearest_feedbacks[:, user])))/self.n_neighbours*2.5
+                # if nearest_feedbacks[:, user].data.shape[0] == 0:
+                #     prediction = 0
+                # else:
+                #     prediction = np.mean(nearest_feedbacks[:, user].data)
+                predictions.append([user, item, prediction])
+
+
+        # items_user = self.collect_items_user_dict(test_data)
+        # predictions = []
+        # for item in items_user:
+        #     dists = np.sort(self.sim_mat_items[item, :])
+        #     num_dists = dists[~np.isnan(dists)]
+        #     nearest_dists = num_dists[-self.n_neighbours:]
+        #     nearest_items_inds = []
+        #     for dist in nearest_dists:
+        #         nearest_items_inds.append(np.where(self.sim_mat_items[item, :] == dist)[0][0])
+        #     nearest_items = self.train_data[np.array(nearest_items_inds), :]
+        #     for user in items_user[item]:
+        #         nearest_ratings = nearest_items[:, user].data
+        #         ratings_inds = nearest_items[:, user].nonzero()[0]
+        #         # print(nearest_dists)
+        #         # print(nearest_ratings - np.mean(nearest_ratings))
+        #         # print(nearest_ratings)
+        #         # print(ratings_inds)
+        #         # print(nearest_dists)
+        #         # print(nearest_dists[ratings_inds])
+        #         # print(np.sum(nearest_dists[ratings_inds]))
+        #         # print('----------------------------------------')
+        #         prediction = np.sum(np.multiply(nearest_dists[ratings_inds], nearest_ratings))/np.sum(nearest_dists[ratings_inds])
+        #         predictions.append([user, item, np.mean(self.train_data[item, :]) if np.isnan(prediction) else prediction])
 
         return np.array(predictions)
