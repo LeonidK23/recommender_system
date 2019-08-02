d3ca80c5647bf20ee052e9da32f20eb2b510d2eb
LeonidK23 <kaida.leonid@gmail.com>
2019-08-02 16:44:30 +0200

    Refactoring

--- main.py
+++ main.py
@@ -3,55 +3,32 @@ import numpy as np
 
 from recommenders.mean_recommender import MeanRecommender
 from recommenders.random_recommender import RandomRecommender
-from recommenders.knn_user_user_recommender import kNNRecommenderUU
-from recommenders.knn_user_item_recommender import kNNRecommenderUI
-from recommenders.knn_item_item_recommender import kNNRecommenderII
+from recommenders.knn_recommender import kNNRecommender
 from auxiliary.cross_validation import cross_validation
 
 X_test = np.genfromtxt(os.path.join("data", "qualifying_blanc.csv"), delimiter=",", dtype=np.int)
 X_train = np.genfromtxt(os.path.join("data", "train.csv"), delimiter=",", dtype=np.int)
 
-# clf_mean = MeanRecommender()
-# clf_mean.fit(X_train)
-# predictions_mean = clf_mean.predict(X_test)
-#
-# clf_rand = RandomRecommender()
-# clf_rand.fit(X_train)
-# predictions_rand = clf_rand.predict(X_test)
-#
-# clf_knn_UU = kNNRecommenderUU(3)
-# clf_knn_UU.fit(X_train)
-# predictions_kNN_UU = clf_knn_UU.predict(X_test)
-#
-# clf_knn_II = kNNRecommenderII(3)
-# clf_knn_II.fit(X_train)
-# predictions_kNN_II = clf_knn_II.predict(X_test)
-#
-# clf_knn_UI = kNNRecommenderUI(9)
-# clf_knn_UI.fit(X_train)
-# predictions_kNN_UI = clf_knn_UI.predict(X_test)
-#
-# clf = RandomRecommender() # Error = 41.5
-# clf = MeanRecommender() # Error = 5.5
-# clf = kNNRecommenderUU(3) # Error = 4.3
-# clf = kNNRecommenderII(3) # Error = appr. 3.7
-clf = kNNRecommenderUI(9) # Error = 1.9
-acc = cross_validation(clf, X_train)
-# print(acc)
+clf_mean = MeanRecommender()
+clf_mean.fit(X_train)
+predictions_mean = clf_mean.predict(X_test)
 
-# np.savetxt("data/qualifying_mean.csv", predictions_mean,
-#            delimiter=",", newline="\n", encoding="utf-8")
-# np.savetxt("data/qualifying_random.csv", predictions_rand,
-#            delimiter=",", newline="\n", encoding="utf-8")
-# np.savetxt("data/qualifying_knn_UU.csv", predictions_kNN_UU,
-#           delimiter=",", newline="\n", encoding="utf-8")
-# np.savetxt("data/qualifying_knn_II.csv", predictions_kNN_II,
-#           delimiter=",", newline="\n", encoding="utf-8")
-# np.savetxt("data/qualifying_knn_UI.csv", predictions_kNN_UI,
-#           delimiter=",", newline="\n", encoding="utf-8")
-# np.savetxt("data/qualifying_hybrid.csv", final_pred,
-#         delimiter=",", newline="\n", encoding="utf-8")
+clf_rand = RandomRecommender()
+clf_rand.fit(X_train)
+predictions_rand = clf_rand.predict(X_test)
 
-# TODO: add train_test_split
-# TODO: cross_validation,
-# TODO: correct packaging(add __init__ file for the project)
+clf_knn = kNNRecommender(9)
+clf_knn.fit(X_train)
+predictions_kNN = clf_knn.predict(X_test)
+
+# clf = RandomRecommender()
+# clf = MeanRecommender()
+# clf = kNNRecommender(9)
+# acc = cross_validation(clf, X_train)
+
+np.savetxt("data/qualifying_mean.csv", predictions_mean,
+           delimiter=",", newline="\n", encoding="utf-8")
+np.savetxt("data/qualifying_random.csv", predictions_rand,
+           delimiter=",", newline="\n", encoding="utf-8")
+np.savetxt("data/qualifying_knn.csv", predictions_kNN,
+          delimiter=",", newline="\n", encoding="utf-8")
---
+++ recommenders/knn_recommender.py
@@ -0,0 +1,93 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
+from datetime import datetime
+
+class kNNRecommender:
+    def __init__(self, k):
+        self.n_neighbours = k
+        self.train_data = 0
+
+    def fit(self, train_data):
+        self.train_data = csr_matrix((train_data[:, 2] + 1, (train_data[:, 0], train_data[:, 1]))).T
+        # self.sim_mat_items = self.compute_pearson(train_data, beta=200)
+        self.sim_mat_items = self.load_pearson()
+        np.fill_diagonal(self.sim_mat_items, -1)
+
+    def load_pearson(self):
+        return np.load("data/pears_corr_20190730-212646.npy")
+
+    def compute_pearson(self, mat, beta=False):
+        mat = csr_matrix((mat[:, 2] + 1, (mat[:, 0], mat[:, 1]))).T
+        mu = np.zeros((mat.shape[0], 1))
+        corr_mat = np.zeros((mat.shape[0], mat.shape[0]))
+        corr_mat[:] = np.nan
+        non_zeros = np.argwhere(mat > 0)                                        # known values indices
+        I_v = []                                                                # each item of I_v is a list of users, which rated the item number I_v.index(item)
+        for i in range(mat.shape[0]):
+            # if there are no ratings mu = nan, otherwise mean value
+            mu[i] = np.nan if mat[i, :].data.shape[0]==0 else np.mean(mat[i, :].data - 1)
+            I_v.append(non_zeros[non_zeros[:, 0]==i][:, 1])
+
+        for i in range(mat.shape[0]):
+            I_u = I_v[i]
+            if np.isnan(mu[i]) == False:                                        # if the item was rated by somebody
+                for j in range(len(I_v)):
+                    I_u_I_v = np.intersect1d(I_u, I_v[j])                       # get similar users
+                    if I_u_I_v.shape[0] > 0:
+                        centered_ratings_u = mat[i, I_u_I_v].data - 1 - mu[i]
+                        centered_ratings_v = mat[j, I_u_I_v].data - 1 - mu[j]
+                        numerator = np.sum(np.multiply(centered_ratings_u, centered_ratings_v))
+                        denumerator = np.sqrt(np.sum(np.square(centered_ratings_u)))* \
+                                      np.sqrt(np.sum(np.square(centered_ratings_u)))
+                        if denumerator == 0:
+                            denumerator = 1e-6
+                        if beta == False:
+                            corr_mat[i, j] = numerator/denumerator
+                        else:
+                            # significance weighting
+                            corr_mat[i, j] = (I_u_I_v.shape[0]/beta)*numerator/denumerator
+                    else:
+                        corr_mat[i, j] = np.nan
+            else:
+                corr_mat[i, :] = np.nan
+
+        timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
+        np.save('data/pears_corr_'+timestr+'.npy', corr_mat)
+
+        return corr_mat
+
+    def collect_items_user_dict(self, test_data):
+        """Create a dictionary of items to predict and corresponding users."""
+        items_to_fill = {}
+        for row in test_data:
+            user = row[0]
+            item = row[1]
+            if item not in items_to_fill:
+                items_to_fill[item] = []
+            items_to_fill[item] += [user.item()]
+
+        return items_to_fill
+
+    def predict(self, test_data):
+        items_user = self.collect_items_user_dict(test_data)
+        predictions = []
+        for item in items_user:
+            distances = np.sort(self.sim_mat_items[item, :])
+            numerical_dists = distances[~np.isnan(distances)]
+            nearest_dists = numerical_dists[-self.n_neighbours:]
+            nearest_items_inds = []
+            for dist in nearest_dists:
+                nearest_items_inds.append(np.where(self.sim_mat_items[item, :] == dist)[0][0])
+            nearest_items = self.train_data[np.array(nearest_items_inds), :]
+            for user in items_user[item]:
+                nearest_ratings = nearest_items[:, user].data - 1
+                if nearest_ratings.shape[0] > 0:
+                    nearest_ratings_inds = nearest_items[:, user].nonzero()[0]
+                    prediction = np.sum(np.multiply(np.square(nearest_dists[nearest_ratings_inds]), \
+                                 nearest_ratings))/np.sum(np.square(nearest_dists[nearest_ratings_inds]))
+                else:
+                    prediction = np.mean(self.train_data[item, :].data)
+                predictions.append([user, item, prediction])
+
+        return np.array(predictions)
